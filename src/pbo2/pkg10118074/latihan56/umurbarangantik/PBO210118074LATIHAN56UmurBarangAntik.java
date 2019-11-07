56536aec53939b50938c375d985c910fcae30cd4
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-11-07 21:45:47 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118074/latihan56/umurbarangantik/PBO210118074LATIHAN56UmurBarangAntik.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author ASUS
+ * NAMA : fikri maulana
+ * KELAS : if2
+ * NIM : 10118074
+ */
+public class PBO210118074LATIHAN56UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Radio radio = new Radio(234);
+        radio.setName("Radio FM");
+        
+        System.out.println("Nama barang Antik : " + radio.getName());
+        radio.tampilUmur();
+        
+        System.out.println("developed by : fikri maulana");
+    }
+    
+}
+    
+
